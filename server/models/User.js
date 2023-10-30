dcd7ac6fe2f9674c8a3d166eb3b64918d5e53a3c
Asiak <asiakemp24@gmail.com>
2023-10-29 20:20:39 -0400

    created user model

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+// import required modules
+const mongoose = requre('mongoose');
+const bcrypt = require('bcrypt');
+
+// define user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+// define prehook method to hash password
+
+// define method to compare and validate password
+
+// define user model
+const User = mongoose.model('User', userSchema);
+
+// export user model
+module.exports = User;
